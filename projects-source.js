9688b841613b1d57773119ae6873b614df84c333
Jim Kang <jimkang@gmail.com>
2017-06-19 18:05:14 -0400

    Implement streaming from local db.

--- projects-source.js
+++ projects-source.js
@@ -2,8 +2,9 @@ var callNextTick = require('call-next-tick');
 var leveljs = require('level-js');
 var levelup = require('levelup');
 var Sublevel = require('level-sublevel');
+var queue = require('d3-queue').queue;
 
-function ProjectsSource({onDeed}) {
+function ProjectsSource({user, onDeed, onProject}) {
   var db = levelup(
     'observatory',
     {
@@ -11,37 +12,72 @@ function ProjectsSource({onDeed}) {
       valueEncoding: 'json'
     }
   );
-  var userDb = Sublevel(db).sublevel('user');  
+  var userDb = Sublevel(db).sublevel('user').sublevel(user);
+  var deedDb = userDb.sublevel('deed');
+  var projectDb = userDb.sublevel('project');
 
   return {
-    putDeed: putDeed,
-    getDeed: getDeed
+    putDeed,
+    getDeed,
+    putProject,
+    getProject,
+    startStream
   };
 
-  function putDeed({deed, user}, done) {
-    getDeedDb(user).put(deed.id, deed, processDeed);
+  function putDeed(deed, done) {
+    put(deedDb, deed, onDeed, done);
+  }
 
-    function processDeed(error) {
-      if (error) {
-        done(error);
-      }
-      else {
-        if (onDeed) {
-          onDeed(deed);
-        }
-        done();
-      }
+  function getDeed(id, done) {
+    deedDb.get(id, done);
+  }
+
+  function putProject(project, done) {
+    put(projectDb, project, onProject, done);
+  }
+
+  function getProject(id, done) {
+    projectDb.get(id, done);
+  }
+
+  function startStream({sources = ['local', 'API']}, done) {
+    var q = queue();
+
+    if (sources.indexOf('local') !== -1) {
+      q.defer(streamLocalEntities, deedDb, onDeed);
+      q.defer(streamLocalEntities, projectDb, onProject);
     }
-  }
 
-  function getDeed({user, id}, done) {
-    getDeedDb(user).get(id, done);
-  }
-
-  function getDeedDb(user) {
-    var currentUserDb = userDb.sublevel(user);
-    return currentUserDb.sublevel('deed');
+    q.awaitAll(done);
   }
 }
+
+function put(db, entity, listener, done) {
+  db.put(entity.id, entity, processEntity);
+
+  function processEntity(error) {
+    if (error) {
+      done(error);
+    }
+    else {
+      if (listener) {
+        listener(entity);
+      }
+      done();
+    }
+  }
+}
+
+function streamLocalEntities(db, listener, done) {
+  if (listener) {
+    var stream = db.createValueStream();
+    stream.on('data', listener);
+    stream.on('end', done);
+  }
+  else {
+    callNextTick(done);
+  }
+}
+
 
 module.exports = ProjectsSource;
--- tests/browser/storage-tests.js
+++ tests/browser/storage-tests.js
@@ -1,10 +1,11 @@
 var test = require('tape');
 var ProjectsSource = require('../../projects-source');
 var assertNoError = require('assert-no-error');
+var queue = require('d3-queue').queue;
+var findWhere = require('lodash.findwhere');
 
 test('Update db with deed.', deedUpdateTest);
-
-// test('Stream repos from db.');
+test('Stream deeds.', localDeedStreamTest);
 
 // test('Update repos in db from API');
 // What should we emit? Commits? Whole repos?
@@ -15,8 +16,10 @@ function deedUpdateTest(t) {
   t.plan(4);
 
   var projectsSource = ProjectsSource({
-    onDeed: checkDeed    
+    user: 'jimkang',
+    onDeed: checkDeed
   });
+
   var deed = {
     'abbreviatedOid': '30a7e8c',
     id: '30a7e8c',
@@ -26,7 +29,8 @@ function deedUpdateTest(t) {
     projectName: 'mishear',
     type: 'commit'
   };
-  projectsSource.putDeed({deed: deed, user: 'jimkang'}, checkPutError);
+
+  projectsSource.putDeed(deed, checkPutError);
 
   function checkDeed(emittedDeed) {
     t.deepEqual(emittedDeed, deed, 'Correct deed is emitted.');
@@ -34,11 +38,121 @@ function deedUpdateTest(t) {
 
   function checkPutError(error) {
     assertNoError(t.ok, error, 'No error while putting deed.');
-    projectsSource.getDeed({user: 'jimkang', project: 'mishear', id: '30a7e8c'}, checkGet);
+    projectsSource.getDeed('30a7e8c', checkGet);
   }
 
   function checkGet(error, gottenDeed) {
     assertNoError(t.ok, error, 'No error while getting deed.');
     checkDeed(gottenDeed);
+  }
+}
+
+function localDeedStreamTest(t) {
+  var shouldListenToEvents = false;
+  var emittedDeeds = [];
+  var emittedProjects = [];
+
+  var projectsSource = ProjectsSource({
+    user: 'jimkang',
+    onDeed: collectDeed,
+    onProject: collectProject
+  });
+
+  var deeds = [
+    {
+      'abbreviatedOid': '30a7e8c',
+      id: '30a7e8c',
+      'message': 'Refactored mishear module to export a createMishear function that sets up  to use the probable given to createMishear.',
+      'committedDate': '2015-10-05T01:42:19Z',
+      'repoName': 'mishear',
+      projectName: 'mishear',
+      type: 'commit'
+    },
+    {
+      'abbreviatedOid': '1d35d45',
+      id: '1d35d45',
+      'message': '1.0.1',
+      'committedDate': '2015-09-28T13:07:55Z',
+      'repoName': 'mishear',
+      projectName: 'mishear',
+      type: 'commit'
+    },
+    {
+      id: '1e273c2',
+      'abbreviatedOid': '1e273c2',
+      'message': 'Nominally working.',
+      'committedDate': '2015-09-26T04:48:26Z',
+      'repoName': 'attnbot',
+      projectName: 'attnbot',
+      type: 'commit'
+    }
+  ];
+
+  var projects = [
+    {
+      'name': 'mishear',
+      'id': 'MDEwOlJlcG9zaXRvcnk0MzEwMTQ0Mg==',
+      'pushedAt': '2015-10-18T16:53:27Z',
+      'description': 'Finds possible mishearings for a given word.',
+      'lastCheckedDate': '2017-06-19T01:39:22.345Z',
+      'weHaveTheOldestCommit': true
+    },
+    {
+      'name': 'attnbot',
+      'id': 'MDEwOlJlcG9zaXRvcnk0MzA2NjIwMA==',
+      'pushedAt': '2015-10-18T17:09:10Z',
+      'description': 'A bot that doesn\'t always hear things correctly.',
+      'lastCheckedDate': '2017-06-19T01:39:22.344Z'
+    }
+  ];
+
+  var q = queue();
+  deeds.forEach(queuePut);
+  projects.forEach(queuePutProject);
+  q.awaitAll(streamDeeds);
+
+  function queuePut(deed) {
+    q.defer(projectsSource.putDeed, deed);
+  }
+
+  function queuePutProject(project) {
+    q.defer(projectsSource.putProject, project);
+  }
+
+  function streamDeeds(error) {
+    assertNoError(t.ok, error, 'No error while putting deeds and projects.');
+    shouldListenToEvents = true;
+    projectsSource.startStream({sources: ['local']}, checkStreamEnd);
+  }
+
+  function collectDeed(deed) {
+    if (shouldListenToEvents) {
+      emittedDeeds.push(deed);
+    }
+  }
+
+  function collectProject(project) {
+    if (shouldListenToEvents) {
+      emittedProjects.push(project);
+    }
+  }
+
+  function checkStreamEnd(error) {
+    assertNoError(t.ok, error, 'No error while streaming local stuff.');
+    t.equal(emittedDeeds.length, deeds.length, 'Correct number of deeds was emitted');
+    deeds.forEach(checkEmittedForDeed);
+    t.equal(emittedProjects.length, projects.length, 'Correct number of projects was emitted.');
+    projects.forEach(checkEmittedForProject);
+    t.end();
+  }
+
+  function checkEmittedForDeed(deed) {
+    var correspondingEmittedDeed = findWhere(emittedDeeds, {id: deed.id});
+    t.deepEqual(correspondingEmittedDeed, deed, 'Emitted deed is correct.');
+  }
+
+  function checkEmittedForProject(project) {
+    var correspondingEmittedProject = findWhere(emittedProjects, {id: project.id});
+    t.deepEqual(correspondingEmittedProject, project, 'Emitted project is correct.');
   }
 }
