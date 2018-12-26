4da17bf7549e5bb3d4876a756c075fe71bedd803
Jim Kang <jimkang@gmail.com>
2018-12-26 08:52:08 -0500

    Refactor category criteria renderering. Render sort and group-by categories as well.

--- dom/render-arrangement-controls.js
+++ dom/render-arrangement-controls.js
@@ -8,51 +8,57 @@ function renderArrangementControls({
   criteria,
   onControlChange
 }) {
-  var filterCriteria = criteria.filter(curry(criterionWorksAs)('filter'));
-  var filterCriteriaByCategory = groupBy(filterCriteria, 'category');
-  var sortByCriteria = criteria.filter(curry(criterionWorksAs)('sortBy'));
-  var groupByCriteria = criteria.filter(curry(criterionWorksAs)('groupBy'));
-  console.log(filterCriteriaByCategory, sortByCriteria, groupByCriteria);
-
   var container = d3.select(containerSelector);
-  var filtersRoot = container.select('.filter-list');
-  var sortsRoot = container.select('.sort-list');
-  var groupBysRoot = container.select('.group-by-list');
+  renderCriteria('filter', 'filter');
+  renderCriteria('sort', 'sort');
+  renderCriteria('group-by', 'groupBy');
 
-  var filterCriteriaCategoryRoots = filtersRoot
-    .selectAll('.filter-category-root')
-    .data(Object.keys(filterCriteriaByCategory), accessor('identity'));
-  filterCriteriaCategoryRoots.exit().remove();
-  var newCategoryRoots = filterCriteriaCategoryRoots
-    .enter()
-    .append('div')
-    .classed('filter-category-root', true);
-  newCategoryRoots.append('h5').classed('category-title', true);
-  newCategoryRoots.append('ul').classed('filter-criteria-root', true);
+  function renderCriteria(criterionType, criterionTypeCamelCase) {
+    var criteriaForCategory = criteria.filter(
+      curry(criterionWorksAs)(criterionTypeCamelCase)
+    );
+    var criteriaByCategory = groupBy(criteriaForCategory, 'category');
+    var root = container.select('.' + criterionType + '-list');
+    var filterCriteriaCategoryRoots = root
+      .selectAll('.' + criterionType + '-category-root')
+      .data(Object.keys(criteriaByCategory), accessor('identity'));
+    filterCriteriaCategoryRoots.exit().remove();
+    var newCategoryRoots = filterCriteriaCategoryRoots
+      .enter()
+      .append('div')
+      .classed('' + criterionType + '-category-root', true);
+    newCategoryRoots.append('h5').classed('category-title', true);
+    newCategoryRoots
+      .append('ul')
+      .classed(criterionType + '-criteria-root', true);
 
-  var currentCategoryRoots = newCategoryRoots.merge(
-    filterCriteriaCategoryRoots
-  );
-  currentCategoryRoots.select('.category-title').text(accessor('identity'));
-  var filterCriteriaRoots = currentCategoryRoots.selectAll(
-    '.filter-criteria-root'
-  );
-  var filterCriteriaForCategory = filterCriteriaRoots
-    .selectAll('.criterion')
-    .data(getCriteriaForCategory, makeCriterionId);
-  filterCriteriaForCategory.exit().remove();
-  filterCriteriaForCategory
-    .enter()
-    .append('li')
-    .classed('criterion', true)
-    .merge(filterCriteriaForCategory)
-    .text(accessor('name'));
+    var currentCategoryRoots = newCategoryRoots.merge(
+      filterCriteriaCategoryRoots
+    );
+    currentCategoryRoots.select('.category-title').text(accessor('identity'));
+    var filterCriteriaRoots = currentCategoryRoots.selectAll(
+      '.' + criterionType + '-criteria-root'
+    );
+    var filterCriteriaForCategory = filterCriteriaRoots
+      .selectAll('.criterion')
+      .data(getCriteriaForCategory, makeCriterionId);
+    filterCriteriaForCategory.exit().remove();
+    filterCriteriaForCategory
+      .enter()
+      .append('li')
+      .classed('criterion', true)
+      .on('click', onCriterionClick)
+      .merge(filterCriteriaForCategory)
+      .text(accessor('name'));
 
-  function getCriteriaForCategory(category) {
-    return filterCriteriaByCategory[category];
+    function getCriteriaForCategory(category) {
+      return criteriaByCategory[category];
+    }
   }
 
-  filtersRoot;
+  function onCriterionClick(criterion) {
+    onControlChange({ criterionSelected: criterion });
+  }
 }
 
 function criterionWorksAs(role, criterion) {
