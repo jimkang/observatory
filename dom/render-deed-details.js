cb3c3341325ff2d9ad82a133231a025ece3ac5d6
Jim Kang <jimkang@gmail.com>
2019-01-14 09:05:26 -0500

    Render links in details box. Use template for details box to DRY things up.

--- dom/render-deed-details.js
+++ dom/render-deed-details.js
@@ -5,6 +5,9 @@ function RenderDeedDetails({ user, detailsLayerSelector = '.details-layer' }) {
   // Pre-rendering prep with static elements.
   var detailsLayer = d3.select(detailsLayerSelector);
   var detailsBox = detailsLayer.select('.details-box');
+  if (detailsBox.empty()) {
+    detailsBox = createDetailsBox(detailsLayer);
+  }
   var closeButton = detailsBox.select('.close-button');
 
   closeButton.on('click', hideBox);
@@ -24,6 +27,13 @@ function RenderDeedDetails({ user, detailsLayerSelector = '.details-layer' }) {
   function hideBox() {
     detailsLayer.classed('destroyed', true);
   }
+}
+
+function createDetailsBox(layer) {
+  var template = document.getElementById('details-box-template');
+  var inst = document.importNode(template.content, true);
+  layer.node().appendChild(inst);
+  return layer.select('.details-box');
 }
 
 module.exports = RenderDeedDetails;
--- dom/render-detail-innards.js
+++ dom/render-detail-innards.js
@@ -1,9 +1,12 @@
+var accessor = require('accessor')();
+
 function renderDeedDetailInnards({ parent, deed, project, user }) {
   var projectDetails = parent.select('.project-details');
   var projectNameLink = projectDetails.select('.name-link');
   var projectStartDate = projectDetails.select('.date.started');
   var projectLastActiveDate = projectDetails.select('.date.last-active');
   var projectDescription = projectDetails.select('.description');
+  var linkList = projectDetails.select('.link-list');
   var deedDetails = parent.select('.deed-details');
   var deedName = deedDetails.select('.name');
   var deedDateLink = deedDetails.select('.date-link');
@@ -16,6 +19,7 @@ function renderDeedDetailInnards({ parent, deed, project, user }) {
   if (project.lastActiveDate) {
     projectLastActiveDate.text(dateStringToDisplayForm(project.lastActiveDate));
   }
+  renderLinks(project.links ? project.links : []);
   projectDescription.text(project.description);
 
   deedDetails.classed('hidden', !deed);
@@ -23,6 +27,18 @@ function renderDeedDetailInnards({ parent, deed, project, user }) {
     deedName.text(deed.message);
     deedDateLink.text(dateStringToDisplayForm(deed.committedDate));
     deedDateLink.attr('href', getDeedLink(user, project, deed));
+  }
+
+  function renderLinks(links) {
+    var items = linkList.selectAll('li').data(links);
+    items.exit().remove();
+    var newItems = items.enter().append('li');
+    newItems.append('a').attr('target', '_blank');
+    var updateItems = newItems.merge(items);
+    updateItems
+      .select('a')
+      .attr('href', accessor('0'))
+      .text(accessor('1'));
   }
 }
 
